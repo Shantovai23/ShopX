19b7286bef193d97e61326a691f4c63b10f99e89
shantovai23 <hasibulhasan23sept@gmail.com>
2021-09-17 21:38:33 +0600

    product card ui added

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Categories from './components/Categories'
 import './App.css';
 function App() {
   return (
-    <div>
+    <div className='main'>
       <UpperNav/>
       <Navbar/>
       <Slider/>
--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,19 +1,47 @@
-import React from 'react'
-import {Container} from 'react-bootstrap'
+import React from "react";
+import { Container, Row, Col, Card } from "react-bootstrap";
+import img from "../img/user.png";
+import ShoppingCartOutlinedIcon from "@material-ui/icons/ShoppingCartOutlined";
 
 const Categories = () => {
-    return (
-        <Container>
-            <div className='categories'>
-            <span>ALL</span>
-             <span>ELECTRONICS</span>
-             <span>JEWELERY</span>
-             <span>MEN'S</span>
-             <span>WOMEN'S</span>
-            </div>
-            <hr className='categories-hr'/>
-        </Container>
-    )
-}
+  return (
+    <Container>
+      <div className="categories">
+        <span>ALL</span>
+        <span>ELECTRONICS</span>
+        <span>JEWELERY</span>
+        <span>MEN'S</span>
+        <span>WOMEN'S</span>
+      </div>
+      <hr className="categories-hr" />
+      <div className='py-5'>
+        <Row>
+          <Col sm={12} md={6} lg={4} xl={3}>
+            <Card border='light' className='card-shadow'>
+              <div className='card-half'>
+                <div>
+                  {/* <i class="far fa-heart"></i> */}
+                </div>
+                <div className='card-second'>
+                  <img src={img} alt="product-img" className='product-img' />
+                </div>
+              </div>
 
-export default Categories
+              <div className='name-price'>
+                 <p className='product-name'>Lava Vtase</p>
+                 <p className='product-price'>$50.00</p>
+              </div>
+              <hr className="categories-hr"/>
+                <div className='cart-div'>
+                <ShoppingCartOutlinedIcon />
+                <span>ADD TO CART</span>
+              </div>
+            </Card>
+          </Col>
+        </Row>
+      </div>
+    </Container>
+  );
+};
+
+export default Categories;
